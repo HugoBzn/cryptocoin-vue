fdbe9468f6a01912f504e5f9f7362dee80afa5aa
HugoBzn <hugobazan1499@gmail.com>
2023-06-27 12:27:51 -0600

    Template ready for API

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <script setup>
+import { ref } from "vue";
 
+const monedas = ref([
+  { codigo: "USD", texto: "Dolar de Estados Unidos" },
+  { codigo: "MXN", texto: "Peso Mexicano" },
+  { codigo: "EUR", texto: "Euro" },
+  { codigo: "GBP", texto: "Libra Esterlina" },
+]);
 </script>
 
 <template>
   <div>
-    
+    <div class="contenedor">
+      <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
+      <div class="contenido">
+        <form class="formulario">{"codigo":"EUR","texto":"Euro"}
+          <div class="campo">
+            <label for="moneda">Moneda:</label>
+            <select id="moneda">
+              <option value="">-- Seleccione --</option>
+              <option v-for="moneda in monedas" :value="moneda.codigo">{{ moneda.texto }}</option>
+            </select>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
