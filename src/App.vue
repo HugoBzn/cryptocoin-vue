b9bc170aaee407494b75a1f4742df5dc37cb127e
HugoBzn <hugobazan1499@gmail.com>
2023-06-27 12:54:33 -0600

    v-model to save data in reactive

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, reactive, onMounted } from "vue";
 
 const monedas = ref([
   { codigo: "USD", texto: "Dolar de Estados Unidos" },
@@ -9,6 +9,11 @@ const monedas = ref([
 ]);
 
 const criptomonedas = ref([]);
+
+const cotizar = reactive({
+  moneda: "",
+  criptomoneda: "",
+});
 
 onMounted(() => {
   const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
@@ -26,7 +31,7 @@ onMounted(() => {
         <form class="formulario">
           <div class="campo">
             <label for="moneda">Moneda:</label>
-            <select id="moneda">
+            <select id="moneda" v-model="cotizar.moneda">
               <option value="">-- Seleccione --</option>
               <option v-for="moneda in monedas" :value="moneda.codigo">{{ moneda.texto }}</option>
             </select>
@@ -34,7 +39,7 @@ onMounted(() => {
 
           <div class="campo">
             <label for="cripto">Criptomoneda:</label>
-            <select id="cripto">
+            <select id="cripto" v-model="cotizar.criptomoneda">
               <option value="">-- Seleccione --</option>
               <option v-for="criptomoneda in criptomonedas" :value="criptomoneda.CoinInfo.Name">
                 {{ criptomoneda.CoinInfo.FullName }}
